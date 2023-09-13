078b8b2dbe9186c802d2fb9d62845affc0e3fc41
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-09-13 12:25:58 -0700

    Create successful element making function

--- src/index.js
+++ src/index.js
@@ -6,52 +6,46 @@ import './style.css';
 
 
 
-function component() {
-    const element = document.createElement(`div`);
-    element.innerHTML = 'To Do List';
-    element.classList.add(`hello`);
-    return element;
-}
 
-document.body.appendChild(component());
+function createEl(array, type){
+    const element = document.createElement(`${type}`);
+    let id = array[0];
 
-
-function createElement([array]){
-    const element = document.createElement(`div`);
-
-    element.setAttribute(`id`, `${array[0]}`);
+    element.setAttribute(`id`, `${id}`);
     element.innerHTML = `something`;
-    if (arguments.length > 1){
-        for (let i = 1; i < arguments.length; i++ ){
-            element.classList.add(`${arguments[i]}`)
+    if (array.length > 1){
+        for (let i = 1; i < array.length; i++ ){
+            element.classList.add(`${array[i]}`)
         }
     }
     return element;
 }
 
-function populateElement(element, content){
+function populateEl(element, content){
     element.innerHTML = content;
     return element;
 }
 
-function placeElement(element, location){
+function placeEl(element, location){
     const locationDiv = document.getElementById(`${location}`);
     locationDiv.appendChild(element);
     return;
 }
 
 
-function enchilada(array, content, location){
-    let element = createElement(array);
-    populateElement(element, content);
-    placeElement(element,location);
+function createElement(array, type, content, location){
+    let element = createEl(array, type);
+    populateEl(element, content);
+    placeEl(element,location);
 }
+//  Create element takes 4 arguments:
+//  1. Array containing first the id, then any class attributes.
+//  2. A string indicating the type of the new element    'div'
+//  3. A string containing the innerhtml content.
+//  4. A string containing the id of the element you'd like to append the new element to. The main container is #content.
 
-console.log(`something`);
-console.log(`something  else`);
-
-enchilada([`kevin`, `frederick`, `boutilier`], "I can't believe it mother fucking worked", "content")
-
+createElement([`kevin`, `frederick`, `boutilier`], `div`, "I can't believe it friggin worked", "content")
+createElement(['sassy', 'boutilier'], 'div', 'Another sentence up on the mafuckin screen', 'content')
 
 
 
