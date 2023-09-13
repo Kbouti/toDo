35128c77dec9f6f544ec99d20e29b7e432df8a89
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-09-13 09:33:10 -0700

    Create divs with function

--- src/index.js
+++ src/index.js
@@ -9,3 +9,28 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+
+function newElement(id){
+    const element = document.createElement(`div`);
+    element.setAttribute(`id`, `${id}`);
+    element.innerHTML = `something`;
+    if (arguments.length > 1){
+        for (let i = 1; i < arguments.length; i++ ){
+            element.classList.add(`${arguments[i]}`)
+        }
+    }
+    document.body.appendChild(element)
+}
+
+console.log(`something else`);
+
+
+newElement(`sassy`);
+newElement(`kevin`, `frederick`, `boutilier`);
+
+//class constructor to make divs
+//homepage dom element loading
+//class constructor to make tasks
+//projects to organize tasks
+//ui to manipulate tasks
