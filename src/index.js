0d94028339e22f20334c3964f6d640841eed09a6
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-09-13 10:29:43 -0700

    Update pseudocode

--- src/index.js
+++ src/index.js
@@ -29,6 +29,13 @@ console.log(`something else`);
 newElement(`sassy`);
 newElement(`kevin`, `frederick`, `boutilier`);
 
+
+
+//create the div (with id and maybe class)
+//put something in it
+//put it where we want it to go
+
+
 //class constructor to make divs
 //homepage dom element loading
 //class constructor to make tasks
