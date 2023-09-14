3997129bcd7bf2975a0fd4309279764c3c105bdb
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-09-14 11:18:59 -0700

    Begin writing task constructor

---
+++ src/taskConstructor.js
@@ -0,0 +1,17 @@
+class Project {
+
+}
+
+class Task {
+
+    constructor(project, title, description, urgency, status){
+
+    }
+
+}
+
+let task1 = new Task(`car`, `Roof Bike Rack`, `Mount fork mount on roof rack. Need brackets to secure fork mount to roof rails`, `low`, `need parts`);
+
+console.log(task1)
+
+
