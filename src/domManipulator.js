e1552852b731a69a548a2f830288db2203da4b49
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-09-21 10:22:03 -0700

    Get and log newTaskName

--- src/domManipulator.js
+++ src/domManipulator.js
@@ -52,27 +52,27 @@ function addEventListeners(projectList){
     const projectForm = document.getElementById(`projectForm`);
     projectForm.addEventListener(`submit`, function(event){
         event.preventDefault();
-        
         const projectNameInput = document.getElementById(`projectNameInput`);
-        
         const newProject = new Project(projectNameInput.value, `no`, []);
         projectList.push(newProject);
-
         const newElement = document.createElement(`div`);
             newElement.setAttribute(`id`, `${projectNameInput.value}`);
             newElement.classList.add(`project`);
             newElement.innerHTML = projectNameInput.value;
             projectsContainer = document.getElementById(`projectsContainer`);
             projectsContainer.appendChild(newElement);
-        
         toggleDisplay(`projectFormDiv`, `flex`);
         projectForm.reset();
-        
     })
 
+    const taskForm = document.getElementById(`taskForm`);
+    taskForm.addEventListener(`submit`, function(event){
+        event.preventDefault();
+        const taskNameInput = document.getElementById(`taskNameInput`);
+        const newTaskName = taskNameInput.value;
+        console.log(newTaskName);
 
-
-
+    })
 }
 
 
