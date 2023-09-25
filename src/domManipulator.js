fb875e14c1a4460099870cca22af85dcf9036156
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-09-24 18:11:25 -0700

    Apply margin to task card elements

--- src/domManipulator.js
+++ src/domManipulator.js
@@ -196,7 +196,7 @@ function makeTaskCard(task, projectList){
                 taskCardLeft.appendChild(projectContainer);
                 const projectLabel = document.createElement(`div`);
                     projectLabel.classList.add(`taskAttributeLabel`);
-                    projectLabel.innerHTML = `Project:`
+                    projectLabel.innerHTML = `Project: `
                     projectContainer.appendChild(projectLabel);
                 const projectValue = document.createElement(`div`);
                     projectValue.classList.add(`taskName`);
@@ -209,7 +209,7 @@ function makeTaskCard(task, projectList){
                 taskCardLeft.appendChild(nameContainer);  
                 const nameLabel = document.createElement(`div`);
                     nameLabel.classList.add(`taskAttributeLabel`);
-                    nameLabel.innerHTML = `Task Name:`
+                    nameLabel.innerHTML = `Task Name: `
                     nameContainer.appendChild(nameLabel);
                 const taskName = document.createElement(`div`);
                     taskName.classList.add(`taskName`);
@@ -221,14 +221,14 @@ function makeTaskCard(task, projectList){
                 taskCardRight.appendChild(descriptionContainer);  
                 const descriptionLabel = document.createElement(`div`);
                     descriptionLabel.classList.add(`taskAttributeLabel`);
-                    descriptionLabel.innerHTML = `Task Description:`
+                    descriptionLabel.innerHTML = `Task Description: `
                     descriptionContainer.appendChild(descriptionLabel);
                 const taskDescription = document.createElement(`div`);
                     taskDescription.classList.add(`taskDescription`);
                     taskDescription.innerHTML = task.description;
                     descriptionContainer.appendChild(taskDescription);
                     
-                    
+
 
 
 
@@ -241,7 +241,7 @@ function makeTaskCard(task, projectList){
                 taskCardLeft.appendChild(urgencyContainer);  
                 const urgencyLabel = document.createElement(`div`);
                     urgencyLabel.classList.add(`taskAttributeLabel`);
-                    urgencyLabel.innerHTML = `Urgency:`
+                    urgencyLabel.innerHTML = `Urgency: `
                     urgencyContainer.appendChild(urgencyLabel);
                 const taskUrgency = document.createElement(`div`);
                     taskUrgency.classList.add(`taskUrgency`);
@@ -264,7 +264,7 @@ function makeTaskCard(task, projectList){
                 taskCardLeft.appendChild(statusContainer);
                 const statusLabel = document.createElement(`div`);
                     statusLabel.classList.add(`taskAttributeLabel`);
-                    statusLabel.innerHTML = `Status`;
+                    statusLabel.innerHTML = `Status: `;
                     statusContainer.appendChild(statusLabel);
                 const taskStatus = document.createElement(`div`);
                     taskStatus.classList.add(`taskStatus`);
