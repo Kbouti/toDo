a275e559c0a6012811ae9c1f711cc40bcc800d62
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-09-25 10:23:10 -0700

    Remove unneeded comments

--- src/taskCards.js
+++ src/taskCards.js
@@ -1,26 +1,17 @@
-// const projectManager = require(`./projectManager.js`);
-// const Task = projectManager.Task;
-// const Project = projectManager.Project;
+const projectManager = require(`./projectManager.js`);
+const Task = projectManager.Task;
+const Project = projectManager.Project;
 
 
 
-// function testTaskCards(projectList){
-// const newTaskSubmitButton = document.getElementById(`newTaskSubmitButton`);
-// newTaskSubmitButton.addEventListener(`click`, function(){
-//     console.log(projectList);
-// })
-// };
+export function testFunction(projectList){
+    console.log(`something happened`);
+    const newTaskSubmitButton = document.getElementById(`newTaskSubmitButton`);
+newTaskSubmitButton.addEventListener(`click`, function(){
+    console.log(projectList);
 
-
-
-// function testFunction(projectList){
-//     console.log(`something happened`);
-//     const newTaskSubmitButton = document.getElementById(`newTaskSubmitButton`);
-// newTaskSubmitButton.addEventListener(`click`, function(){
-//     console.log(projectList);
-
-// })}
+})}
 
 
 // export default testFunction;
-// do the work here that's done in domManipulator line 100
+//make task card function here instead of dom manipulator
